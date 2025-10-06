03aed37b68527f9170dfe32b0aa79315c8307ed1
Wesley Maik <wesleynamikaze404@gmail.com>
2025-10-05 20:49:31 -0400

    fix(api): change create user dto

--- apps/api/src/modules/users/dto/create-user.dto.ts
+++ apps/api/src/modules/users/dto/create-user.dto.ts
@@ -1,12 +1,6 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
-  @IsString()
-  id: string;
-  @IsNotEmpty()
-  createdAt: Date;
-  @IsNotEmpty()
-  updatedAt: Date;
   @IsNotEmpty()
   name: string;
   @IsEmail()
