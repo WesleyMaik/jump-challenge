87666331c6dc6cace07f15a024ec538f7c89b30c
Wesley Maik <wesleynamikaze404@gmail.com>
2025-10-06 12:20:44 -0400

    fix(api): add global prefix and enable cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,6 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
+  app.setGlobalPrefix('api');
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
